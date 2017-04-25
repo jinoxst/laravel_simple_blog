8627a57ca0754b4e94c402c187843833af949a3a
jinoxst <jinoxst@gmail.com>
2017-04-25 13:41:27 +0900

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+// App::bind('App\Billing\Stripe', function(){
+
+
+// $stripe = App::make('App\Billing\Stripe');
+// dd(resolve('App\Billing\Stripe'));
+
+Route::get('/tasks', 'TasksController@index');
+Route::get('/tasks/{task}', 'TasksController@show');
+
+Route::get('/', 'PostsController@index')->name('home');
+Route::get('/posts/create', 'PostsController@create');
+Route::post('/posts', 'PostsController@store');
+Route::get('/posts/{post}', 'PostsController@show');
+Route::post('/posts/{post}/comments', 'CommentsController@store');
+Route::get('/register', 'RegistrationController@create');
+Route::post('/register', 'RegistrationController@store');
+Route::get('/login', ['as' => 'login', 'uses' => 'SessionsController@create']);
+Route::post('/login', ['as' => 'login', 'uses' => 'SessionsController@store']);
+Route::get('/logout', 'SessionsController@destroy');
+
+Route::get('/posts/tags/{tag}', 'TagsController@index');
---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Post;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ExampleTest extends TestCase
+{
+	use DatabaseTransactions;
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        // $this->assertTrue(true);
+        $first = factory(Post::class)->create();
+        $second = factory(Post::class)->create([
+        	'created_at' => \Carbon\Carbon::now()->subMonth()
+        ]);
+        $post = Post::archives();
+        // dd($post);
+        $this->assertEquals([
+        	[
+        		'year' => $first->created_at->format('Y'),
+        		'month' => $first->created_at->format('F'),
+        		'published' => 1
+        	],
+        	[
+        		'year' => $second->created_at->format('Y'),
+        		'month' => $second->created_at->format('F'),
+        		'published' => 1
+        	],
+        ], $post);
+    }
+}
